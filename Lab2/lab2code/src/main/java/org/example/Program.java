8f993ca48911c5c021085aecd298422ea56e6aae
mykola2004 <144995714+mykola2004@users.noreply.github.com>
2023-10-25 17:37:49 +0200

    stage 2

--- Lab2/lab2code/src/main/java/org/example/Program.java
+++ Lab2/lab2code/src/main/java/org/example/Program.java
@@ -2,7 +2,36 @@ package org.example;
 
 public class Program {
     public class Matrix {
+        private int rows;
+        private int columns;
+        private double[][] data;
+
+        public Matrix() {
+            this.rows = 0;
+            this.columns = 0;
+            this.data = new double[0][0];
+        }
+
+        public Matrix(int rows, int columns) {
+            this.rows = rows;
+            this.columns = columns;
+            this.data = new double[rows][columns];
+        }
+
+        public Matrix(Matrix other) {
+            this.rows = other.getRows();
+            this.columns = other.getColumns();
+            this.data = new double[rows][columns];
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    this.data[i][j] = other.get(i, j);
+                }
+            }
+        }
+
         
+
     }
 
 }
