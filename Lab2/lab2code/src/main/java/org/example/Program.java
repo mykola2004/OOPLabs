6321cdcb6151bebdc353d46c5c1377306e24c7ca
mykola2004 <144995714+mykola2004@users.noreply.github.com>
2023-10-25 17:12:55 +0200

    created class Matrix

--- Lab2/lab2code/src/main/java/org/example/Program.java
+++ Lab2/lab2code/src/main/java/org/example/Program.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Program {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public class Matrix {
+        
     }
+
 }
