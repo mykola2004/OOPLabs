6bf271430b0e61bc18d47adedb9ceae35a668123
mykola2004 <144995714+mykola2004@users.noreply.github.com>
2023-11-06 12:38:49 +0100

    stage 2

--- Lab2/lab2code/src/main/java/org/example/Main.java
+++ Lab2/lab2code/src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.regex.MatchResult;
 
 public class Main {
     public static void main(String[] args) {
-        Matrix mat1 = new Matrix();
+        org.example.Matrix mat1 = new org.example.Matrix();
         //System.out.println(mat1.fillMatrix());
     }
 }
--- Lab2/lab2code/src/main/java/org/example/Matrix.java
+++ Lab2/lab2code/src/main/java/org/example/Matrix.java
@@ -1,9 +1,34 @@
 package org.example;
+import java.util.Scanner;
 
 public class Matrix {
     private int rows;
     private int columns;
     private double[][] data;
+
+    public Matrix(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.data = new double[rows][columns];
+    }
+
+    public Matrix() {
+        this.rows = 0;
+        this.columns = 0;
+        this.data = new double[0][0];
+    }
+
+    public Matrix(Matrix other) {
+        this.rows = other.getRows();
+        this.columns = other.getColumns();
+        this.data = new double[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                this.data[i][j] = other.get(i, j);
+            }
+        }
+    }
+
 
 
 }
