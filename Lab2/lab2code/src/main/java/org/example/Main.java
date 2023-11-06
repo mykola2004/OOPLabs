f1cb3e205e3f5978751c9ae6ba0ae360e5352883
mykola2004 <144995714+mykola2004@users.noreply.github.com>
2023-11-06 13:02:43 +0100

    stage 4

--- Lab2/lab2code/src/main/java/org/example/Main.java
+++ Lab2/lab2code/src/main/java/org/example/Main.java
@@ -1,11 +1,19 @@
-
+package org.example;
 import org.example.Matrix;
 
 import java.util.regex.MatchResult;
 
 public class Main {
     public static void main(String[] args) {
-        org.example.Matrix mat1 = new org.example.Matrix();
-        //System.out.println(mat1.fillMatrix());
+        org.example.Matrix mat1 = new org.example.Matrix(2,2);
+        System.out.println(mat1.getRows());
+        System.out.println(mat1.getColumns());
+        System.out.println(mat1.get(1,0));
+        mat1.fillMatrix();
+        System.out.println(mat1.get(1,0));
+        org.example.Matrix mat2 = new org.example.Matrix(mat1);
+        System.out.println(mat2.getRows());
+        System.out.println(mat2.getColumns());
+        System.out.println(mat2.get(1,1));
     }
 }
--- Lab2/lab2code/src/main/java/org/example/Matrix.java
+++ Lab2/lab2code/src/main/java/org/example/Matrix.java
@@ -15,7 +15,7 @@ public class Matrix {
     public Matrix() {
         this.rows = 0;
         this.columns = 0;
-        this.data = new double[0][0];
+        this.data = new double[][]{{0}};
     }
 
     public Matrix(Matrix other) {
@@ -28,17 +28,40 @@ public class Matrix {
             }
         }
     }
-    
-    public void fillMatrix() {
-        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                System.out.print("Enter the value for cell (" + i + ", " + j + "): ");
-                double value = scanner.nextDouble();
-                data[i][j] = value;
+    public void fillMatrix() {
+        if (rows == 0 && columns == 0){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the value for cell (" + 0 + ", " + 0 + "): ");
+            double value = scanner.nextDouble();
+            data[0][0] = value;
+        }
+        else{
+            Scanner scanner = new Scanner(System.in);
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    System.out.print("Enter the value for cell (" + i + ", " + j + "): ");
+                    double value = scanner.nextDouble();
+                    data[i][j] = value;
+                }
             }
         }
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public double get(int row, int column) {
+        //if (row < 0 || row >= rows || column < 0 || column >= columns) {
+        //    throw new IllegalArgumentException("Invalid row or column index");
+        //}
+        return data[row][column];
     }
 
 
